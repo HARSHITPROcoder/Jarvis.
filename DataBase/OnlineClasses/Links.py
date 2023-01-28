c060b371dcd7def83a03ed04b6e12e10c3fe3a57
Hrashit2611/ <100829059+HARSHITPROcoder@users.noreply.github.com>
2023-01-28 15:56:13 +0530

    Add files via upload

---
+++ DataBase/OnlineClasses/Links.py
@@ -0,0 +1,36 @@
+
+def Science():
+
+    Link = "https://meet.google.com/gfy-puyd-ixn"
+
+    return Link
+
+def Maths():
+
+    Link = "https://meet.google.com/gfy-puyd-ixn"
+
+    return Link
+
+def Hindi():
+
+    Link = "https://meet.google.com/ywx-uaua-jqc"
+
+    return Link
+
+def English():
+
+    Link = "https://meet.google.com/ywx-uaua-jqc"
+
+    return Link
+
+def sst():
+
+    Link = "https://meet.google.com/ywx-uaua-jqc"
+
+    return Link
+
+def sanskrit():
+    
+    Link = "https://meet.google.com/ywx-uaua-jqc"
+    
+    return Link
